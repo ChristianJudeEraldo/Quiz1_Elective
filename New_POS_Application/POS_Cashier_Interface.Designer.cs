03dba8c4d776bb1d03836fb35a289162588c8014
Christian Jude Eraldo <christian.eraldo@lpunetwork.edu.ph>
2025-02-27 16:16:45 +0800

    this final output for quiz 1

--- New_POS_Application/POS_Cashier_Interface.Designer.cs
+++ New_POS_Application/POS_Cashier_Interface.Designer.cs
@@ -1485,9 +1485,11 @@
             // 
             // POS_Cashier_Interface
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1540, 845);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.breakfastBrn);
             this.Controls.Add(this.dinnerBtn);
             this.Controls.Add(this.groupBox1);
